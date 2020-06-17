50642c8839d243e8cb8476b35dd6a96432b98207
olebeck <psolebeck@gmail.com>
2020-06-17 21:13:51 +0200

    inital-commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import datetime
+import os
+try:
+    os.mkdir("files")
+except FileExistsError:
+    pass
+VHBB_UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"
+r = requests.get('https://rinnegatamante.it/vitadb/list_hbs_json.php',headers={"user-agent":VHBB_UA}).json()
+
+for entry in r:
+    titleid = entry["titleid"]
+    name = entry["name"]
+    count = entry["downloads"]
+    date = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    l = (titleid,name,count,date)
+    try:
+        f = open(f"files/{titleid}.csv","x")
+    except:
+        f = open(f"files/{titleid}.csv","a")
+    for i in list(l):
+        try:
+            f.write(str(i))
+        except UnicodeEncodeError:
+            f.write("unicode sucks")
+        if l.index(i) != len(l) -1:
+            f.write(",")
+    f.write("\n")
+    f.close()
